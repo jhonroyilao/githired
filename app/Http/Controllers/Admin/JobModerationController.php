93e7a5413b0387f9c901e440e09ca11ba50e82af
Frances Lorraine <franceslorrainepmontemayor@iskolarngbayan.pup.edu.ph>
2026-06-24 23:30:19 +0800

    Created controller for admit moderation

---
+++ app/Http/Controllers/Admin/JobModerationController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Enums\JobStatus;
+use App\Http\Controllers\Controller;
+use App\Models\JobListing;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class JobModerationController extends Controller
+{
+    
+}
